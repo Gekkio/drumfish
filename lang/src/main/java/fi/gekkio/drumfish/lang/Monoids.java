5fb77d0d21249c5533a51973ff337d8165e7895e
Joonas Javanainen <joonas.javanainen@gmail.com>
2013-02-18 09:14:01 +0200

    New: Monoid law verification method

--- lang/src/main/java/fi/gekkio/drumfish/lang/Monoids.java
+++ lang/src/main/java/fi/gekkio/drumfish/lang/Monoids.java
@@ -3,6 +3,12 @@ package fi.gekkio.drumfish.lang;
 import java.io.ObjectStreamException;
 import java.io.Serializable;
 
+import lombok.val;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Supplier;
+
 public final class Monoids {
 
     private Monoids() {
@@ -54,6 +60,34 @@ public final class Monoids {
 
     public static Monoid<Integer> integerProduct() {
         return IntegerProduct.INSTANCE;
+    }
+
+    public static <T> void verifyMonoidLaws(Monoid<T> monoid, Supplier<T> generator) {
+        {
+            // Associativity
+            val a = generator.get();
+            val b = generator.get();
+            val c = generator.get();
+
+            val r1 = monoid.mappend(monoid.mappend(a, b), c);
+            val r2 = monoid.mappend(a, monoid.mappend(b, c));
+
+            Preconditions.checkState(Objects.equal(r1, r2),
+                    "Monoid is not associative: (a • b) • c != a • (b • c). Counter-example: (%s • %s) • %s == %s, %s == %s • (%s • %s)", a, b, c, r1, r2, a,
+                    b, c);
+        }
+
+        {
+            // Identity element
+            val a = generator.get();
+            val i = monoid.mempty();
+
+            val r1 = monoid.mappend(i, a);
+            val r2 = monoid.mappend(a, i);
+
+            Preconditions.checkState(Objects.equal(r1, a), "Monoid identity element is invalid: i • a != a. Counter-example: %s • %s == %s", i, a, r1);
+            Preconditions.checkState(Objects.equal(r2, a), "Monoid identity element is invalid: a • i != a. Counter-example: %s • %s == %s", a, i, r2);
+        }
     }
 
 }
